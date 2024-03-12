7e4a017924a9495e775a531ecddf7c06f66e64e7
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-03-12 14:42:34 -0300

    Update app.rb

--- POO/Animalia/app.rb
+++ POO/Animalia/app.rb
@@ -15,4 +15,13 @@ mouse = Mammal.new("Rattus", "Rato", "Onívoro", "Diversos")
 hipoppotamus = Mammal.new("Hippopotamus amphibius", "Hipopótamo", "Onívoro", "Lagos, rios e pântanos")
 
 #Repitale animals
-
+king_cobra = Repitale.new("Ophiophagus hannah", "Cobra-real", "Carnívoro", "Florestas tropicais")
+green_sea_turtle = Repitale.new("Chelonia mydas", "Tartaruga-verde", "Herbívoro", "Oceanos tropicais")
+nile_crocodile = Repitale.new("Crocodylus niloticus", "Crocodilo-do-nilo", "Carnívoro", "Lagos e rios")
+panther_chameleon = Repitale.new("Furcifer pardalis", "Camaleão-pantera", "Onívoro", "Florestas tropicais")
+green_iguana = Repitale.new("Iguana iguana", "Iguana-verde", "Herbívoro", "Florestas tropicais")
+tuatara = Repitale.new("Sphenodon punctatus", "Tuatara", "Onívoro", "Ilhas da Nova-Zelândia")
+corn_snake = Repitale.new("Pantherophis guttatus", "Serpente-do-milho", "Carnívoro", "Diversos na América do Norte")
+eastern_box_turtle = Repitale.new("Terrapene carolina", "Tartaruga-de-Caixa", "Onívoro", "Florestas")
+softshell_turtle = Repitale.new("Trionychidae", "Cágado-de-carapaça-mole", "Onívoro", "Lagos, rios e pântanos")
+gila_monster = Repitale.new("Heloderma suspectum", "Lagarto-monstro-de-gila", "Carnívoro", "Desertos")
