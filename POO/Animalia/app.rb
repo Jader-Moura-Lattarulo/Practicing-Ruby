ca457b8893c33f73660b96bf281c108f1c1ff6c3
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-03-12 14:09:09 -0300

    update

--- POO/Animalia/app.rb
+++ POO/Animalia/app.rb
@@ -2,15 +2,17 @@ require_relative 'class_animal.rb'
 require_relative 'animal_mammal.rb'
 require_relative 'animal_repitale.rb'
 
+#Mammal animals
 lion = Mammal.new("Panthera leo", "Leão", "Carnívoro", "Savana")
 dog = Mammal.new("Canis lupus familiaris", "Cachorro", "Onívoro", "Diversificado")
 dolphin = Mammal.new("Tursiops truncatus", "Golfinho", "Carnívoro", "Oceanos e mares tropicais")
 polar_bear = Mammal.new("Ursus maritimus", "Urso polar", "Carnívoro", "Regiões árticas")
-orangutan = Mammal.new("Pongo pygmaeus", "Orangotango", "Frugívero", "Florestas tropicais")
-fruit.bat = Mammal.new("", "", "", "")
-giraffe = Mammal.new("", "", "", "")
-capuchin_monkey = Mammal.new("", "", "", "")
-mouse = Mammal.new("", "", "", "")
-hipoppotamus = Mammal.new("", "", "", "")
+orangutan = Mammal.new("Pongo pygmaeus", "Orangotango", "Frugívoro", "Florestas tropicais")
+fruit_bat = Mammal.new("Artibeus", "Morcego", "Frugívoro", "Diversificado")
+giraffe = Mammal.new("Giraffa camelopardalis", "Girafa", "Herbívoro", "Savana")
+capuchin_monkey = Mammal.new("Sapajus", "Macaco-prego", "Onívoro", "Florestas tropicais")
+mouse = Mammal.new("Rattus", "Rato", "Onívoro", "Diversos")
+hipoppotamus = Mammal.new("Hippopotamus amphibius", "Hipopótamo", "Onívoro", "Lagos, rios e pântanos")
 
-leao.show_data
+#Repitale animals
+
