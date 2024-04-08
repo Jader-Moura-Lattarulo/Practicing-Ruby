3c9a11e55eadb4c442e20fec8f67a7c3a190c57a
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-04-08 19:31:57 -0300

    update

--- POO/Clash_Royale_Cards/classes/class_common_cards.rb
+++ POO/Clash_Royale_Cards/classes/class_common_cards.rb
@@ -1,10 +1,10 @@
 require_relative 'class_cards.rb'
 
 class Common < Card
-    def initialize(type, rarity, elixir, name, level, health_points, damage, damage_per_second, targets, range, impact_speed, speed_of_movement)
-        super(rarity, elixir, name, level, health_points, damage, damage_per_second, targets, range, impact_speed, speed_of_movement)
+    def initialize(type, elixir, name, level, health_points, damage, damage_per_second, targets, range, impact_speed, speed_of_movement)
+        super(type, elixir, name, level, health_points, damage, damage_per_second, targets, range, impact_speed, speed_of_movement)
     end
 
-    @type = "Comum"
+    @rarity = "Comum"
     
 end
